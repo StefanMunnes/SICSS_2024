a1363fcd869b35b551e92416da4e01af0a94f19f
Kaylee <kayleetmatheny@gmail.com>
2024-07-09 10:17:28 +0200

    Update

--- exercises/KMatheny/trial.R
+++ exercises/KMatheny/trial.R
@@ -1,1 +1,19 @@
-# first line of code 
+setwd("C:/Users/kayle/Dropbox/Resources/SICSS_2024/exercises/KMatheny")
+credentials::set_github_pat("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")
+
+install.packages("RSelenium")
+install.packages("wdman")
+install.packages("netstat")
+
+library(RSelenium)
+library(wdman)
+library(netstat)
+
+selenium()
+
+selenium_object <- selenium(retcommand = T, check = F)
+
+remote_driver <- rsDriver(browser = "chrome",
+                          chromever = "126.0.6478.126",
+                          verbose = FALSE,
+                          port = free_port())
