53ffe4719f5c006cfc395be52dd62318ebb413f3
boris-css <>
2024-07-10 15:43:52 +0200

    data wrangling exercise

--- exercises/BN/cleaning_dataframe.R
+++ exercises/BN/cleaning_dataframe.R
@@ -14,4 +14,41 @@ cnn_data <- cnn_data[c("title", "author 1", "author 2", "author 3", "author 4", 
 published_or_updated <- str_extract_all(cnn_data$timestamp, "Published|Updated")
 
 #extract weekday
-weekday <- str_extract_all(cnn_data$timestamp, "(?<=EDT, )|(|?<=EST, )\\w{3}")
+weekday <- str_extract_all(cnn_data$timestamp, "(?<=EDT, )\\w{3}|(?<=EST, )\\w{3}")
+
+#extract date
+date_strings <- str_extract_all(cnn_data$timestamp, "\\w+ \\d{1,2}, \\d{4}")
+print(date_strings)
+
+#convert to date object
+date_objects <- mdy(date_strings)
+print(date_objects)
+
+#convert the date format
+formatted_date <- format(date_objects, "%e.%m.%Y")
+
+#trim spaces
+formatted_date <- str_replace_all(formatted_date, " ", "")
+print(formatted_date)
+
+
+#convert to data frame
+date_df <- data.frame(date = formatted_date)
+
+
+#remove excessive white spaces in titles
+cnn_data$title <- str_replace_all(cnn_data$title, "\\s{2,}|\\n|^ | $", "")
+
+
+#remove excessive white spaces in bodies
+cnn_data$body <- str_replace_all(cnn_data$body, "\\s{2,}|\\n|^ | $", "")
+
+
+#remove all capitalization from titles
+cnn_data$title <- str_to_lower(cnn_data$title, "\\w")
+
+
+#remove all capitalization from bodies
+cnn_data$body <- str_to_lower(cnn_data$body, "\\w")
+
+
