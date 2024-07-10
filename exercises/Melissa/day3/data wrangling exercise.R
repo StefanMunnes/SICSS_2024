e87472420268d60a72eb90646b906e9f9d567a18
melissa.panlasigui <panlasigui@ad.wzb.eu>
2024-07-10 21:42:13 +0200

    asdfasd

--- exercises/Melissa/day3/data wrangling exercise.R
+++ exercises/Melissa/day3/data wrangling exercise.R
@@ -1,13 +1,6 @@
 library(tidyverse)
 
 manip_data <- CNN_cleaning %>%
-  separate_wider_delim(authors,delim = ";",names_sep = "", too_few = "align_start")
-
-rexp <- "^(\\w+)\\s?(.*)$" 
-
-manip_data1 <- manip_data %>%
-  separate_wider_delim(timestamp,delim = "   ",names = c("status","timestamp"), too_many = "merge")%>%
-  separate_wider_delim(timestamp,delim = ",", names = c("discard","date"), too_many = "merge")%>%
+  separate_wider_delim(authors,delim = ";",names_sep = "", too_few = "align_start")%>%
   mutate(across(where(is.character), str_trim))%>%
-  separate_wider_regex(cols = date,patterns = c(weekday = "\\w{3}","\\s",new_date = ".+"),too_few = "align_start")
-
+  separate_wider_regex(cols = timestamp,patterns = c(status = ".+","\\s+.+,\\s",weekday = "\\w{3}","\\s",new_date = ".+"))
