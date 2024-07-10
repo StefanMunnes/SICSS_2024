b108645b06c685a4313c4bcd646f27d7985341f1
melissa.panlasigui <panlasigui@ad.wzb.eu>
2024-07-10 10:03:42 +0200

    scrape

--- exercises/Melissa/day2_webdata_MP/scripts/2_1_scraping_example_MP.R
+++ exercises/Melissa/day2_webdata_MP/scripts/2_1_scraping_example_MP.R
@@ -2,33 +2,65 @@ library(tidyverse)
 library(rvest)
 library(stringi)
 
-html = read_html("https://time.com/search/?q=election&page=2")
+base_html = "https://time.com/search/?q=election&page="
+pages <- paste0(base_html,1:5)
+print(pages)
 
-urls <- html %>%
-  html_nodes(".headline") %>%
-  html_elements("a") %>%
-  html_attr("href")
+all_article_urls <- tibble()
 
-df.urls <- tibble(urls)
+for (page in pages) {
+  url <- read_html(page)
+  
+  article_urls <- url %>%
+    html_nodes(".headline") %>%
+    html_elements("a") %>%
+    html_attr("href") %>%
+    list()
+  
+  all_article_urls <- rbind(all_article_urls,article_urls)
+  
+}
 
-pg_nums <- c[1:5]
+print(all_article_urls)
+ 
+#df.urls <- tibble(urls)
 
+get_article_info = function(html) {
+  url <- read_html("https://time.com/6995779/france-macron-disastrous-election/")
+  
+  title <- url %>%
+    html_nodes("header") %>%
+    html_elements("h1")%>%
+    html_text()
+  
+  author <- url %>%
+    html_node(".inline-block .font-bold") %>%
+    html_text()
+  
+  date <- url %>%
+    html_node("time") %>%
+    html_attr("datetime") %>%
+    str_sub(1,10)
+  
+  text <- url %>%
+    html_nodes("p.self-baseline") %>%
+    html_text()
+  
+  new_article_info <- tibble(title, author, date, text)
+  
+  Sys.sleep(2)
+}
 
+colnames(all_article_urls)[1] <- "all_article_urls"
+article_data <- tibble()
 
-title <- html %>%
-  html_nodes("header") %>%
-  html_elements("h1")%>%
-  html_text()
+for (i in 1:45) {
+  html <- all_article_urls[i]
+  new_data <- get_article_info(html)
+  
+  df.new_data <- tibble(new_data)
+  
+  rbind(article_data,new_data)
+  
+}
 
-author <- html %>%
-  html_node(".inline-block .font-bold") %>%
-  html_text()
-
-date <- html %>%
-  html_node("time") %>%
-  html_attr("datetime") %>%
-  str_sub(1,10)
-
-text <- html %>%
-  html_nodes("p.self-baseline") %>%
-  html_text()
