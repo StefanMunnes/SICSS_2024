feaf9676924c695ae100b815e30413e388815ce4
Stefan Munnes <munnes@wzb.eu>
2024-07-23 11:02:02 +0200

    Revert "d"
    This reverts commit f1fcf2c1662421cfa118e505375064227b558fdf.

--- exercises/Jing Zhou/tfidf.R
+++ exercises/Jing Zhou/tfidf.R
@@ -56,7 +56,7 @@ library(factoextra)
 tfidf_matrix <- as.matrix(dfm_pp_tfidf)
 
 
-k <- 10
+k <- 20
 
 
 set.seed(123) 
@@ -77,59 +77,3 @@ data_cnn$cluster <- kmeans_result$cluster
 
 
 head(data_cnn)
-
-#lda
-library(quanteda)
-
-library(LDAvis)
-library(ggplot2)
-library(wordcloud)
-library(quanteda.textmodels)
-library(seededlda)
-
-
-lda_model <- textmodel_lda(dfm_pp, k = 10)
-
-
-
-topic_terms <- terms(lda_model, 50)
-topic_terms
-
-
-
-# 提取主题术语分布
-phi <- lda_model$phi
-
-phi
-# 提取文档主题分布
-theta <- lda_model$theta
-
-theta
-# 计算每个文档的长度（文档中词汇的总数）
-doc_length <- rowSums(dfm_pp)
-
-doc_length
-# 词汇表
-vocab <- colnames(dfm_pp)
-
-# 词汇频率
-term_frequency <- colSums(dfm_pp)
-term_frequency
-
-# 准备LDAvis所需的数据
-lda_data <- list(
-  phi = phi,
-  theta = theta,
-  doc.length = doc_length,
-  vocab = vocab,
-  term.frequency = term_frequency
-)
-
-# 创建LDAvis可视化
-json_lda <- createJSON(phi = lda_data$phi,
-                       theta = lda_data$theta,
-                       doc.length = lda_data$doc.length,
-                       vocab = lda_data$vocab,
-                       term.frequency = lda_data$term.frequency)
-
-serVis(json_lda, out.dir = 'vis', open.browser = TRUE)
